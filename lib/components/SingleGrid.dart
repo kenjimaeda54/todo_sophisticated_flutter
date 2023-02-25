745435dfda7c9dada449b93fa56d22d1a2e12143
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-02-25 16:45:35 -0300

    feat: finalized logic know if user add ou not on card

--- lib/components/SingleGrid.dart
+++ lib/components/SingleGrid.dart
@@ -1,37 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:todo_sophisticated/utils/ColorsConstants.dart';
+import '../model/ListImg.dart';
 
 class SingleGrid extends HookWidget {
-  late String image;
-  late String title;
-  SingleGrid({Key? key, required this.title, required this.image})
+  late final ListImg listImg;
+  late Function(ListImg item) handleSelectedItem;
+  List<ListImg> istenSelected;
+
+  SingleGrid(
+      {Key? key,
+      required this.listImg,
+      required this.istenSelected,
+      required this.handleSelectedItem})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    handleNavigation() {}
-
     return InkWell(
-      onTap: handleNavigation,
+      onTap: () => handleSelectedItem(listImg),
       splashColor: Colors.transparent,
       highlightColor: Colors.transparent,
       hoverColor: Colors.transparent,
       child: Column(children: [
-        SizedBox(
+        Container(
           height: 80,
           width: 80,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: istenSelected.contains(listImg)
+                    ? ColorsConstants.blue100
+                    : ColorsConstants.whiteColor,
+                width: 1,
+              )),
           child: CircleAvatar(
             backgroundColor: ColorsConstants.whiteColor,
-            child:
-                Image.asset(image, height: 50, width: 50, fit: BoxFit.contain),
+            child: Image.asset(listImg.img,
+                height: 50, width: 50, fit: BoxFit.contain),
           ),
         ),
         const SizedBox(
           height: 10,
         ),
         Text(
-          title,
+          listImg.title,
           style: const TextStyle(
               fontWeight: FontWeight.w400,
               fontSize: 17,
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -8,7 +8,9 @@ import 'package:todo_sophisticated/utils/ColorsConstants.dart';
 
 class HomeScreen extends HookWidget {
   HomeScreen({Key? key}) : super(key: key);
-  final statusBarHeight = window.viewPadding.top;
+  final statusBarTopHeight = window.viewPadding.top;
+  final statusBarBottonHeight = window.viewPadding.bottom;
+  List<ListImg> listItemsSelected = [];
 
   final List<ListImg> listImg = [
     ListImg(id: 1, img: "assets/img/bike.png", title: "Cycling"),
@@ -32,65 +34,113 @@ class HomeScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    var itensSelected = useState(List<ListImg>.empty());
     handleNavigation() {
       print("ola");
     }
 
+    handleSelectItem(ListImg item) {
+      // o useState trabalha com conceito de imutabilidade então não pode ir diretamente  e alterar o estado,
+      // precisa mudar a lista que esta na refreencia é igualar o valor com oque esta no estado,
+      // muitos paradigmas funcionam dessa maneira pois trabalhamos com conceito de observer,
+      // exemplo em    em redux , pois  não podemos altera  uma lista que esta no redux diretamente,precisa manipular ela
+      // e igualar ao valor do estate
+      // state.data = data.fitler(it => it.id === item.id) // isso e em redux
+      itensSelected.value.contains(item)
+          ? itensSelected.value = itensSelected.value
+              .where((element) => element.id != item.id)
+              .toList()
+          : itensSelected.value = [...itensSelected.value, item];
+    }
+
     return Scaffold(
         backgroundColor: ColorsConstants.primaryColor,
-        body: ListView.builder(
-            itemCount: listHeader.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: statusBarHeight + 5, horizontal: 20),
-                child: Column(
-                  children: [
-                    const Text(
-                      "Chosse Activity",
-                      textAlign: TextAlign.center,
+        body: Stack(
+          children: [
+            ListView.builder(
+                itemCount: listHeader.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      top: statusBarTopHeight,
+                      left: 20,
+                      right: 20,
+                      bottom: statusBarBottonHeight + 150,
+                    ),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Chosse Activity",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: ColorsConstants.secondaryColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        const Text(
+                          "You can chosse more than one",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: ColorsConstants.blue50,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const SizedBox(
+                          height: 19,
+                        ),
+                        const Divider(
+                          thickness: 2,
+                          indent: 19,
+                          color: ColorsConstants.whiteColor100,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        GridView.count(
+                            shrinkWrap: true,
+                            physics: const ScrollPhysics(),
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 50,
+                            crossAxisSpacing: 5,
+                            children: listImg
+                                .map((it) => SingleGrid(
+                                      listImg: it,
+                                      istenSelected: itensSelected.value,
+                                      handleSelectedItem: handleSelectItem,
+                                    ))
+                                .toList())
+                      ],
+                    ),
+                  );
+                }),
+            Positioned(
+                width: MediaQuery.of(context).size.width,
+                bottom: MediaQuery.of(context).size.height * 0.1,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: MediaQuery.of(context).size.width * 0.22),
+                  child: ElevatedButton(
+                    onPressed: handleNavigation,
+                    style: ElevatedButton.styleFrom(
+                        elevation: 13,
+                        backgroundColor: ColorsConstants.secondaryColor,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 20)),
+                    child: const Text(
+                      "Get Start",
                       style: TextStyle(
-                          color: ColorsConstants.secondaryColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                        color: ColorsConstants.whiteColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
                     ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    const Text(
-                      "You can chosse more than one",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: ColorsConstants.blue50,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w400),
-                    ),
-                    const SizedBox(
-                      height: 19,
-                    ),
-                    const Divider(
-                      thickness: 2,
-                      indent: 19,
-                      color: ColorsConstants.whiteColor100,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    GridView.count(
-                        shrinkWrap: true,
-                        physics: const ScrollPhysics(),
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 50,
-                        crossAxisSpacing: 5,
-                        children: listImg
-                            .map((it) => SingleGrid(
-                                  image: it.img,
-                                  title: it.title,
-                                ))
-                            .toList())
-                  ],
-                ),
-              );
-            }));
+                  ),
+                ))
+          ],
+        ));
   }
 }
